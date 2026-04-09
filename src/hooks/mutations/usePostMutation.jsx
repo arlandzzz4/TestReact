f60ce5b3a65ba34bdfda0addb95b8980e641307a
LAPTOP-A2N8H58Q\admin <spd9817@gmail.com>
2026-04-09 14:58:01 +0900

    0409뮤테이션수정

--- src/hooks/mutations/usePostMutation.jsx
+++ src/hooks/mutations/usePostMutation.jsx
@@ -1,15 +1,11 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 import { deletePost } from '@/api/postApi';
 
 export const useDeletePostMutation = () => {
-    const queryClient = useQueryClient();
     return useMutation({
-        mutationFn: ({ postId, userEmail }) => deletePost(postId, userEmail),
+        mutationFn: (data) => deletePost(data),
         onSuccess: () => {
             alert('게시글이 성공적으로 삭제되었습니다.');
-            // 'myPosts' 쿼리를 사용하는 모든 곳의 데이터를 무효화하여 리페치를 유도합니다.
-            // MyPage에서 useQuery를 사용하도록 리팩토링하면 더욱 효과적입니다.
-            queryClient.invalidateQueries({ queryKey: ['myPosts'] });
         },
         onError: (error) => {
             console.error('게시글 삭제 에러:', error);
