f7712c5a23505da7fc15167fa4852cf0d4e675f0
gwon <arlandzzz4@gmail.com>
2026-04-10 12:09:20 +0900

    footer수정

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -8,7 +8,7 @@ const AppFooter = () => {
         {/* <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
           CoreUI
         </a> */}
-        <p>Information Of Balance</p>
+        <span>Information Of Balance</span>
         <span className="ms-1">&copy; 2026 Sisa IT</span>
       </div>
       <div className="ms-auto">
