685f2a27768eefd8d0c210979cf1a9e87fbb656a
gwon <arlandzzz4@gmail.com>
2026-04-14 15:10:01 +0900

    getImageUrl 추가

--- src/constants/properties.js
+++ src/constants/properties.js
@@ -4,6 +4,12 @@ export const properties = {
     const url = import.meta.env.VITE_API_BASE_URL;
     return url && url !== 'api' ? url : '/';
   },
+  getImageUrl: () => {
+    if(import.meta.env.DEV)
+      return import.meta.env.VITE_API_BASE_URL
+    else
+      return '';
+  },
   isDev: import.meta.env.DEV,
   // 개발 환경에서만 로그를 남기고 싶을 때 사용하는 유틸
   log: (...args) => {
